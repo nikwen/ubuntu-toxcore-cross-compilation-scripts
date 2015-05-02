bec07617a1f455e66f5fdb370fd0a85561287bbe
Niklas Wenzel <nikwen.developer@gmail.com>
2015-05-03 00:26:17 +0200

    Create include directory in build all script as well

--- build-all-in-chroot.sh
+++ build-all-in-chroot.sh
@@ -8,14 +8,18 @@ for ARCH in ${ARCHITECTURES[@]}; do
   ./build-toxcore-in-chroot.sh $ARCH
 done
 
-# Create libs directory
+# Create libs and include directories
 
 SCRIPT_DIR=$(dirname $(readlink -f "$0"))
 BUILD_DIR=$SCRIPT_DIR/build
 LIBS_DIR=$BUILD_DIR/libs
+INCLUDE_DIR=$BUILD_DIR/include/tox
 
 rm -rf $LIBS_DIR
 mkdir -p $LIBS_DIR
+
+rm -rf $INCLUDE_DIR
+mkdir -p $INCLUDE_DIR
 
 # Copy libs into desired directory structure
 
@@ -24,3 +28,8 @@ for ARCH in ${ARCHITECTURES[@]}; do
   cp -d $BUILD_DIR/$ARCH/libs/libsodium/lib/libsodium.so* $LIBS_DIR/$ARCH
   cp -d $BUILD_DIR/$ARCH/libs/toxcore/lib/libtoxcore.so* $LIBS_DIR/$ARCH
 done
+
+# Copy include files into include directory
+
+cp $BUILD_DIR/armhf/libs/toxcore/include/tox/tox.h $INCLUDE_DIR
+cp $BUILD_DIR/armhf/libs/toxcore/include/tox/tox_old.h $INCLUDE_DIR
